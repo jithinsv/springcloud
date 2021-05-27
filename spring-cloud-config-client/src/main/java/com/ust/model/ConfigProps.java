34227df6e181973178636dd71ff3f70b1cf0ae42
ustjava13 <ustjava13@java41.iiht.tech>
2021-05-27 13:06:37 +0530

    pushed the config client file

---
+++ spring-cloud-config-client/src/main/java/com/ust/model/ConfigProps.java
@@ -0,0 +1,17 @@
+package com.ust.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties("credentials")
+@Component
+public class ConfigProps {
+	private String username;
+	private String password;
+	private String attempt;
+	
+
+}
